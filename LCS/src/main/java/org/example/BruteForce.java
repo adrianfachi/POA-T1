4e1e20dd83bbd6c0d867f33ca9cdbd5fcdac7a37
Joao Campanher <jvitorcampanher@gmail.com>
2026-09-22 14:42:16 -0300

    Fix brute force solution from substring to subsequence and add iteration counter

--- LCS/src/main/java/org/example/BruteForce.java
+++ LCS/src/main/java/org/example/BruteForce.java
@@ -1,28 +1,22 @@
 package org.example;
 
 public class BruteForce {
-    public static int cont = 0, precont = 0;
-    public static boolean seq = false;
+    public static int iteracoes = 0;
 
     public int lcs(String str1, String str2) {
-        lcsrec(str1.toCharArray(), str2.toCharArray(), 0, 0);
-        return cont;
+        iteracoes = 0;
+        int resultado = lcsRec(str1, str2, 0, 0);
+        System.out.println("Número de iteracoes: " + iteracoes);
+        return resultado;
     }
 
-    private int lcsrec(char[] str1, char[] str2, int i1, int i2) {
-        if(i2 >= str2.length) return cont;
-        if(i1 >= str1.length) return lcsrec(str1, str2, 0, i2 + 1);
-        if(!seq) precont = 0;
-        if(str1[i1] == str2[i2]) {
-            seq = true;
-            precont = 1 + lcsrec(str1, str2, i1 + 1, i2 + 1);
-            if(precont > cont) cont = precont;
-            return precont;
-        }
-        precont = 0;
-        seq = false;
-
-        lcsrec(str1, str2, i1 + 1, i2);
-        return cont;
+    private int lcsRec(String str1, String str2, int i, int j) {
+        iteracoes++;
+        if (i == str1.length() || j == str2.length()) return 0;
+        if (str1.charAt(i) == str2.charAt(j)) return 1 + lcsRec(str1, str2, i + 1, j + 1);
+        return Math.max(
+            lcsRec(str1, str2, i + 1, j),
+            lcsRec(str1, str2, i, j + 1)
+        );
     }
 }
--- LCS/src/main/java/org/example/DynamicProgramming.java
+++ LCS/src/main/java/org/example/DynamicProgramming.java
@@ -2,13 +2,17 @@ package org.example;
 
 public class DynamicProgramming {
     private Integer[][] memo;
+    public static int iteracoes = 0;
 
     public int lcs(String str1, String str2) {
         memo = new Integer[str1.length()][str2.length()];
-        return lcsRec(str1, str2, 0, 0);
+        int resultado = lcsRec(str1, str2, 0, 0);
+        System.out.println("Número de iteracoes: " + iteracoes);
+        return resultado;
     }
 
     private int lcsRec(String str1, String str2, int i, int j) {
+        iteracoes++;
         if (i == str1.length() || j == str2.length()) return 0;
         if (memo[i][j] != null) return memo[i][j];
         if (str1.charAt(i) == str2.charAt(j)) memo[i][j] = 1 + lcsRec(str1, str2, i + 1, j + 1);
